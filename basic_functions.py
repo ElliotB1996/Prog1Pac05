5576f3d9e42decf16b24d3421267d319effa427d
elliotb1996 <elliot.blair@my.jcu.edu.au>
2016-03-30 12:42:56 +1000

    Added comment with my name

--- basic_functions.py
+++ basic_functions.py
@@ -1,11 +1,8 @@
-
-
-
-
 """
 CP1404/CP5632 Workshop 04
 Basic functions
 demonstrating various parameters, returns and the use of a main function
+Cool comment by Elliot Blair <---
 """
 __author__ = 'Lindsay Ward'
 
@@ -18,12 +15,16 @@ def main():
 
 def get_limits():
     minimum = int(input("Enter the minimum: "))
-    maximum = int(input("Enter the maximum: "))
+    maximum = int(input("Enter the maximum ({} or above): ".format(minimum)))
+    while minimum > maximum:
+        print("Maximum to low!")
+        maximum = int(input("Enter the maximum ({} or above): ".format(minimum)))
     return minimum, maximum
 
 
 def print_between(start, end):
-    for i in range(start, end+1):
+    for i in range(start, end + 1):
         print(i, end=' ')
+
 
 main()
